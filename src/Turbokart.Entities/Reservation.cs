268643b9b7029930bdf5004cb04243e007cec577
Tobsi <61752@edu.campusvejle.dk>
2024-10-23 04:21:23 -0500

    The Reservation form is done

---
+++ src/Turbokart.Entities/Reservation.cs
@@ -0,0 +1,33 @@
+﻿namespace Turbokart.Entities
+{
+    public class Reservation
+    {
+        private int _id;
+        private string _name;
+        private string _email;
+        private string _phone;
+        private int _peopleAttending;
+        private DateTime _currentDate;
+        private DateTime _raceDate;
+
+        public Reservation() { }
+
+        public Reservation(string name, string email, string phone, int peopleAttending, DateTime currentDate, DateTime raceDate)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            PeopleAttending = peopleAttending;
+            CurrentDate = currentDate;
+            RaceDate = raceDate;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Phone { get => _phone; set => _phone = value; }
+        public int PeopleAttending { get => _peopleAttending; set => _peopleAttending = value; }
+        public DateTime CurrentDate { get => _currentDate; set => _currentDate = value; }
+        public DateTime RaceDate { get => _raceDate; set => _raceDate = value; }
+    }
+}
--- src/Turbokart.Web/Pages/Booking.cshtml.cs
+++ src/Turbokart.Web/Pages/Booking.cshtml.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+using Turbokart.Entities;
 
 namespace Turbokart.Web.Pages
 {
     public class BookingModel : PageModel
     {
+        // Booking properties
+        public Reservation UserReservation = new();
+
+        // Callendar properties
         public DateTime CurrentDate { get; set; }
         public DateTime FirstDayOfMonth { get; set; }
         public int DaysInMonth { get; set; }
@@ -22,6 +28,28 @@ namespace Turbokart.Web.Pages
 
             FirstDayOfMonth = new DateTime(CurrentDate.Year, CurrentDate.Month, 1);
             DaysInMonth = DateTime.DaysInMonth(CurrentDate.Year, CurrentDate.Month);
+        }
+
+        public void SubmitReservation()
+        {
+            UserReservation.Email = Request.Form["Email"];
+            UserReservation.Name = Request.Form["Name"];
+            UserReservation.Phone = Request.Form["PhoneNumber"];
+            UserReservation.PeopleAttending = int.Parse(Request.Form["Attendees"]);
+
+            UserReservation.CurrentDate = DateTime.Now; // I could use the CurrentDate property, but I'm using DateTime.Now so I also get the timestamp
+            UserReservation.RaceDate = DateTime.Now.AddDays(2); // I need to get the selected date from the calendar
+
+            // Call the database and save the reservation
+            // DB.MakeReservation(UserReservation);
+
+            // Show the user they have successfully made a reservation
+            //ViewData["submit-msg"] = $"Du har booket en reservation den {UserReservation.RaceDate.ToString(new CultureInfo("da-dk"))}\n" +
+            //    $"Navn: {UserReservation.Name}\n" +
+            //    $"Email: {UserReservation.Email}\n" +
+            //    $"Phone: {UserReservation.Phone}\n" +
+            //    $"Attendees: {UserReservation.PeopleAttending}";
+
         }
     }
 }
