344e489a2c179954970692f3eae2f48f2f4d0e17
AspIT-61752 <61752@edu.campusvejle.dk>
2024-10-24 10:54:24 +0200

    The API can now make reservations

--- src/Turbokart.API/Controllers/BookingController.cs
+++ src/Turbokart.API/Controllers/BookingController.cs
@@ -1,25 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
+using Turbokart.DataAccess;
 using Turbokart.Entities;
 
 namespace Turbokart.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class BookingController : Controller
+    public class BookingController(IReservationRepository repo) : Controller
     {
+        private readonly IReservationRepository _db = repo;
+
         [HttpPost]
         [Route("MakeReservation")]
         public IActionResult MakeReservation([FromBody] Reservation userReservation)
         {
             // Call the database and save the reservation
-            Console.WriteLine($"Du har booket en reservation den {userReservation.RaceDate.ToString(new CultureInfo("da-dk"))}\n" +
-                $"Navn: {userReservation.Name}\n" +
-                $"Email: {userReservation.Email}\n" +
-                $"Phone: {userReservation.Phone}\n" +
-                $"Attendees: {userReservation.PeopleAttending}");
 
-            return Ok(200);
+            if (userReservation.PeopleAttending > 0)
+            {
+                try
+                {
+                    /*
+                        {
+                          "id": 0,
+                          "name": "Martin",
+                          "email": "martin-martinili@gmail.com",
+                          "phone": "+4520145632",
+                          "peopleAttending": 5,
+                          "currentDate": "2024-10-24T08:37:39.737Z",
+                          "raceDate": "2024-10-26T08:37:39.737Z"
+                        }
+                     */
+                    Console.WriteLine($"Du har booket en reservation den {userReservation.RaceDate.ToString(new CultureInfo("da-dk"))}\n" +
+                        $"Navn: {userReservation.Name}\n" +
+                        $"Email: {userReservation.Email}\n" +
+                        $"Phone: {userReservation.Phone}\n" +
+                        $"Attendees: {userReservation.PeopleAttending}");
+
+                    _db.Add(userReservation);
+
+                    return Ok(200);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            return BadRequest();
         }
     }
 }
